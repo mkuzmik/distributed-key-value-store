a2e33f1267db03855452b30a1a53bcd11ced895d
Mateusz Kuzmik <mkuzmik@akamai.com>
2017-12-30 12:39:31 +0100

    "Get All" functionality in node implemented

--- src/store_node.erl
+++ src/store_node.erl
@@ -12,10 +12,10 @@
 -behavior(gen_server).
 
 %% API
--export([start_link/0, init/1, handle_call/3, put/2, get/1, handle_cast/2]).
+-export([get_all_from_node/0, get_all_from_node/1, get_all/0, start_link/0, init/1, handle_call/3, put/2, get/1, handle_cast/2]).
 
 start_link() ->
-  logger:info("gen_server: Starting"),
+  logger:info("store_node: Starting"),
   gen_server:start_link({global, node()}, ?MODULE, [], []).
 
 put(Key, Value) ->
@@ -28,16 +28,36 @@ get(Key) ->
   logger:info("router: Forwarding get. key: ~w to node: ~w", [Key, Node]),
   gen_server:call({global, Node}, {get, Key}).
 
+get_all_from_node(Node) ->
+  gen_server:call({global, Node}, {all_from_node}).
+
+get_all_from_node() ->
+  get_all_from_node(node()).
+
+get_all() ->
+  logger:info("Getting ALL command started"),
+  AllNodes = lists:sort(lists:concat([nodes(), [node()]])),
+  Result = lists:foldl(fun (Node, Result) ->
+    logger:info("Getting ALL command in progress. Getting from node: ~w", [Node]),
+    NodeMap = get_all_from_node(Node),
+    maps:merge(NodeMap, Result) end, #{}, AllNodes),
+  logger:info("Getting ALL command completed"),
+  Result.
+
 init([]) ->
   {ok, #{}}.
 
 handle_call({put, Key, Value}, _From, State) ->
-  logger:info("gen_server: Putting key: ~w value: ~w", [Key, Value]),
+  logger:info("store_node: Putting key: ~w value: ~w", [Key, Value]),
   {reply, ok, maps:put(Key, Value, State)};
 
 handle_call({get, Key}, _From, State) ->
-  logger:info("gen_server: Getting key: ~w", [Key]),
-  {reply, get_if_exists(Key, State), State}.
+  logger:info("store_node: Getting key: ~w", [Key]),
+  {reply, get_if_exists(Key, State), State};
+
+handle_call({all_from_node}, _From, State) ->
+  logger:info("store_node: Getting ALL from this node"),
+  {reply, State, State}.
 
 get_if_exists(Key, Map) ->
    case maps:is_key(Key, Map) of
